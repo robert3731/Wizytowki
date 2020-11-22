63b66129e961a27737b7b5e40b833287daa7bfe1
Robert Chaim <robak3731@gmail.com>
2020-11-22 11:16:34 +0100

    modified class BusinessContact

--- wizytowki.py
+++ wizytowki.py
@@ -18,10 +18,18 @@ class BaseContact:
 
 
 class BusinessContact(BaseContact):
-    def __init__(self, company, job, *args, **kwargs):
+    def __init__(self, company, job, business_phone, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.company = company
         self.job = job
+        self.business_phone = business_phone
+
+    def contact(self):
+        i = input('Zadzwonić pod numer prywatny czy firmowy?')
+        if i == 'prywatny':
+            print('Wybieram numer {} i dzwonię do {} {}'.format(self.phone, self.first_name, self.last_name))
+        elif i == 'firmowy':
+            print('Wybieram numer {} i dzwonię do {} {}'.format(self.business_phone, self.first_name, self.last_name))
 
 
 def create_contacts(kind, quantity):
@@ -31,12 +39,15 @@ def create_contacts(kind, quantity):
             cards.append(card)
     elif kind == 'business':
         for i in range(quantity):
-            card = BusinessContact(fake.company(), fake.job(),
+            card = BusinessContact(fake.company(), fake.job(), fake.phone_number(),
                                    fake.first_name(), fake.last_name(), fake.phone_number(), fake.email())
             cards.append(card)
 
 
-cards = []
+card1 = BaseContact('Anna', 'Dymna', '684 201 351', 'a_dymna12@op.pl')
+card2 = BusinessContact('Romp', 'Clock repairer','889 456 632', 'Jolanta', 'Kozłowska', '796 267 177','JolantaKozlowska@armyspy.com')
+
+cards = [card1, card2]
 
 
 def menu():
@@ -54,7 +65,11 @@ def menu():
         elif action == "1":
             by_last_name = sorted(cards, key=lambda c: c.last_name)
             for card in by_last_name:
-                print('-{} {} - {}'.format(card.last_name, card.first_name, card.phone))
+                if isinstance(card, BusinessContact):
+                    print('-{} {}, numer prywatny: {}, numer firmowy: {}'
+                          .format(card.last_name, card.first_name, card.phone, card.business_phone))
+                else:
+                    print('-{} {}, numer prywatny: {}'.format(card.last_name, card.first_name, card.phone))
 
         elif action == '2':
             a = 'base'
