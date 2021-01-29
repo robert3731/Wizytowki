c778d490f52a3c16cd622c8767f7debad9ed43f0
Robert Chaim <robak3731@gmail.com>
2021-01-29 18:11:22 +0100

    Added call function

--- wizytowki.py
+++ wizytowki.py
@@ -9,12 +9,15 @@ class BaseContact:
         self.phone = phone
         self.mail = mail
 
+    def __str__(self):
+        return f"Base Card: {self.last_name} {self.first_name}, e-mail:{self.mail}, phone:{self.phone}\n"
+
     @property
     def label_length(self):
-        return len(self.first_name) + len(self.last_name) + 1
+        return len(self.last_name) + len(self.first_name) + 1
 
-    def contact(self):
-        print('Wybieram numer {} i dzwonię do {} {}'.format(self.phone, self.first_name, self.last_name))
+    def base_contact(self):
+        print('\nCalling {} {} on phone number: {}\n'.format(self.first_name, self.last_name, self.phone))
 
 
 class BusinessContact(BaseContact):
@@ -24,62 +27,79 @@ class BusinessContact(BaseContact):
         self.job = job
         self.business_phone = business_phone
 
-    def contact(self):
-        i = input('Zadzwonić pod numer prywatny czy firmowy?')
-        if i == 'prywatny':
-            print('Wybieram numer {} i dzwonię do {} {}'.format(self.phone, self.first_name, self.last_name))
-        elif i == 'firmowy':
-            print('Wybieram numer {} i dzwonię do {} {}'.format(self.business_phone, self.first_name, self.last_name))
+    def __str__(self):
+        return f"Business Card: {self.last_name} {self.first_name}, e-mail:{self.mail}, phone:{self.phone}\n" \
+               f"\t\t\tCompany:{self.company}, Job:{self.job}, Business phone: {self.business_phone}\n"
+
+    def business_contact(self):
+        print('\nCalling {} {} on phone number:{}\n'.format(self.first_name, self.last_name, self.business_phone))
 
 
-def create_contacts(kind, quantity):
-    if kind == 'base':
-        for i in range(quantity):
-            card = BaseContact(fake.first_name(), fake.last_name(), fake.phone_number(), fake.email())
-            cards.append(card)
-    elif kind == 'business':
-        for i in range(quantity):
-            card = BusinessContact(fake.company(), fake.job(), fake.phone_number(),
-                                   fake.first_name(), fake.last_name(), fake.phone_number(), fake.email())
-            cards.append(card)
+def create_base_contact(quantity):
+    for i in range(quantity):
+        card = BaseContact(fake.first_name(), fake.last_name(), fake.phone_number(), fake.email())
+        cards.append(card)
+
+
+def create_business_contact(quantity):
+    for i in range(quantity):
+        card = BusinessContact(fake.company(), fake.job(), fake.phone_number(),
+                               fake.first_name(), fake.last_name(), fake.phone_number(), fake.email())
+        cards.append(card)
 
 
 card1 = BaseContact('Anna', 'Dymna', '684 201 351', 'a_dymna12@op.pl')
-card2 = BusinessContact('Romp', 'Clock repairer','889 456 632', 'Jolanta', 'Kozłowska', '796 267 177','JolantaKozlowska@armyspy.com')
+card2 = BusinessContact('Romp', 'Clock repairer', '889 456 632',
+                        'Jolanta', 'Kozłowska', '796 267 177', 'JolantaKozlowska@armyspy.com')
 
 cards = [card1, card2]
 
 
+def get_cards():
+    by_last_name = sorted(cards, key=lambda c: c.last_name)
+    for card in by_last_name:
+        print(card)
+
+
+def call(last_name):
+    for card in cards:
+        if card.last_name == last_name:
+            if isinstance(card, BusinessContact):
+                action = input("Which number do you want to call? private/business")
+                if action == 'private':
+                    return card.base_contact()
+                elif action == 'business':
+                    return card.business_contact()
+            elif isinstance(card, BaseContact):
+                return card.base_contact()
+            else:
+                return 'Something went wrong'
+    else:
+        return f'Sorry you don have {last_name} in your cards.'
+
+
 def menu():
     while True:
-        print("1-Show Cards")
-        print("2-Add new base card")
-        print("3-Add new business card")
-        print("4-Exit")
-
+        print("1-Show Cards\n2-Add new base card\n3-Add new business card\n4-Call\n5-Exit")
         action = input("What would you like to do?")
 
-        if action == "4":
-            return False
-
-        elif action == "1":
-            by_last_name = sorted(cards, key=lambda c: c.last_name)
-            for card in by_last_name:
-                if isinstance(card, BusinessContact):
-                    print('-{} {}, numer prywatny: {}, numer firmowy: {}'
-                          .format(card.last_name, card.first_name, card.phone, card.business_phone))
-                else:
-                    print('-{} {}, numer prywatny: {}'.format(card.last_name, card.first_name, card.phone))
+        if action == "1":
+            get_cards()
 
         elif action == '2':
-            a = 'base'
-            b = input('How many cards would you like to add:')
-            create_contacts(a, int(b))
+            quantity = input('How many cards would you like to add:')
+            create_base_contact(int(quantity))
 
         elif action == '3':
-            a = 'business'
-            b = input('How many cards would you like to add:')
-            create_contacts(a, int(b))
+            quantity = input('How many cards would you like to add:')
+            create_business_contact(int(quantity))
+
+        elif action == '4':
+            last_name = input('Who you want to call (type in last name)?')
+            call(last_name)
+
+        elif action == "5":
+            return False
 
         else:
             print('Please choose available option.')
